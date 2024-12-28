2d9f6b0664a608c7de92e3ff1f9aeb4946de5933
RubenHC <ehuacasiccama@gmail.com>
2024-12-28 18:39:26 -0500

    Se realizo el cherry-pick

--- prueba.py
+++ prueba.py
@@ -1,3 +1,17 @@
 print("Hola Mundo")
 def saludar(nombre):
+<<<<<<< HEAD
     print(nombre, "Hola");
+=======
+    print(nombre, "Hola");
+
+def despedir(nombre):
+    print(nombre, "Adios");
+
+def main():
+    saludar("Juan")
+    despedir("Juan")
+
+def bienvenida():
+    print("Bienvenido a Python")
+>>>>>>> b79338d (Funciond de bienvenida)
